8742d4294755a001fe2f5b5b2eb0576631f9e926
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-08-26 00:20:24 +0200

    bootstrap Script/ Css / Nav

--- toDoList/src/app/app.module.ts
+++ toDoList/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
