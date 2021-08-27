e3fabe040706361995f81f9701d63e3c5c1d4be7
Diagne Axel <axel.diagne2021@campus-eni.fr>
2021-08-27 15:12:30 +0200

    Clean Nav and Add todo component

--- toDoList/src/app/todo/todo.component.ts
+++ toDoList/src/app/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 
+
 @Component({
     selector: "my-todo",
     templateUrl: "./todo.component.html",
@@ -7,5 +8,30 @@ import { Component } from "@angular/core";
 })
 
 export class TodoComponent {
+   todos = [
+    {
+      todoName : "Projet 1",
+      todoStatus : true,
+      todoImage : "https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+    },
+    {
+      todoName : "Projet 2",
+      todoStatus : true,
+      todoImage : "https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+    },
+    {
+      todoName : "Projet 3",
+      todoStatus : false,
+      todoImage : "https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+    },
+    {
+      todoName : "Projet 4",
+      todoStatus : false,
+      todoImage : "https://images.unsplash.com/photo-1485550409059-9afb054cada4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
+    }
+  ]
 
+  onChangeStatus(i :number) {
+    this.todos[i].todoStatus = !this.todos[i].todoStatus;
+  }
 }
